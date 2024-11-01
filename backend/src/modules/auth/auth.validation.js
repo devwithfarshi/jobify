8979f12b7abc96dab8c0088a1d9ddf72798a0af8
Salman Farshi <devwithfarshi@gmail.com>
2024-11-01 23:21:10 +0600

    ✨:update: Company routed updated

--- backend/src/modules/auth/auth.validation.js
+++ backend/src/modules/auth/auth.validation.js
@@ -3,6 +3,7 @@ const Joi = require("joi");
 
 const createAccount = {
   body: Joi.object().keys({
+    name: Joi.string().optional(),
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
     role: Joi.string().valid(superAdmin, admin).default("admin"),
